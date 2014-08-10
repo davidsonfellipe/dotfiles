f9e317ea534eaea317fef8280806b34e6620cd81
Davidson Fellipe <email@fellipe.com>
2014-08-10 13:23:23 -0300

    separating installation of node packages

---
+++ node/global.sh
@@ -0,0 +1,55 @@
+source common/functions.sh
+
+# nodejs
+if which node &> /dev/null; then
+    msg_checking "node"
+else
+    msg_install "node" "git clone git://github.com/ry/node.git"
+    brew install node
+    echo "OK"
+fi
+
+# npm
+if which npm &> /dev/null; then
+    msg_checking "npm"
+else
+    msg_install "npm" "curl https://npmjs.org/install.sh | sh"
+    curl -L https://npmjs.org/install.sh | sh
+    echo "OK"
+fi
+
+# grunt-cli
+if which grunt &> /dev/null; then
+    msg_checking "grunt"
+else
+    msg_install "grunt" "npm install -g grunt-cli"
+    sudo npm install -g grunt-cli
+    echo "OK"
+fi
+
+# vtop
+if which vtop &> /dev/null; then
+    msg_checking "vtop"
+else
+    msg_install "vtop" "sudo npm install vtop -g"
+    sudo npm install vtop -g
+    echo "OK"
+fi
+
+# jshint
+if which jshint &> /dev/null; then
+    msg_checking "jshint"
+else
+    msg_install "jshint" "sudo npm install jshint -g"
+    sudo npm install jshint -g
+    echo "OK"
+fi
+
+# csslint
+if which csslint &> /dev/null; then
+    msg_checking "csslint"
+else
+    msg_install "csslint" "sudo npm install -g csslint"
+    sudo npm install -g csslint
+    msg_ok "OK"
+fi
