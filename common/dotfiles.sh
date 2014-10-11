b0771213dd12a5fd433b437e4c86df8cfd8a248a
Davidson Fellipe <email@fellipe.com>
2014-10-11 01:59:12 -0300

    changing copy files to symbolic links

--- common/dotfiles.sh
+++ common/dotfiles.sh
@@ -3,39 +3,39 @@ source common/functions.sh
 # .ssh/config
 if [ -f "ssh/config" ]; then
     msg_installing "~/.ssh/config"
-    cp ssh/config ~/.ssh/config
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/ssh/config ~/.ssh/config
 fi
 
 # .gitconfig
 if [ -f "git/.gitconfig" ]; then
     msg_installing "~/.gitconfig"
-    cp git/.gitconfig ~/.gitconfig
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/git/.gitconfig ~/.gitconfig
 fi
 
 # .editorconfig
 if [ -f ".editorconfig" ]; then
     msg_installing "~/.editorconfig"
-    cp .editorconfig ~/.editorconfig
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/.editorconfig ~/.editorconfig
 fi
 
 # .osx
 if [ -f "osx/.osx" ]; then
     msg_installing "~/.osx"
-    cp osx/.osx ~/.osx
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/osx/.osx ~/.osx
     killall Finder
 fi
 
 # .gitignore_global
 if [ -f "git/.gitignore_global" ]; then
     msg_installing "~/.gitignore_global"
-    cp git/.gitignore_global ~/.gitignore_global
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/git/.gitignore_global ~/.gitignore_global
     git config --global core.excludesfile ~/.gitignore_global
 fi
 
 # .profile
 if [ -f ".bash_profile" ]; then
     msg_installing ".bash_profile"
-    cp .bash_profile ~/.bash_profile
+    sudo ln -s /Users/davidson/Dropbox/Labs/dotfiles/.bash_profile ~/.bash_profile
     source ~/.bash_profile
     msg_alert "For use on this session, type: source ~/.bash_profile"
 fi
