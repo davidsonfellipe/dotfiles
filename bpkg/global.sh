ec230f840b4654f5205ccf8923d73a4360bc4c63
Davidson Fellipe <email@fellipe.com>
2015-02-16 10:37:14 -0500

    [bpkg] adding wifi-password

--- bpkg/global.sh
+++ bpkg/global.sh
@@ -6,3 +6,12 @@ else
     curl -Lo- http://get.bpkg.io| bash
     msg_ok "OK"
 fi
+
+# wifi-password
+if which wifi-password &> /dev/null; then
+    msg_checking "wifi-password"
+else
+    msg_install "wifi-password" "bpkg install rauchg/wifi-password"
+    bpkg install rauchg/wifi-password
+    msg_ok "OK"
+fi
