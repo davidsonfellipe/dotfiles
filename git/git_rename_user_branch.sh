2f698b71b94c92355801e1eec21ad49bb37bb826
Davidson Fellipe <email@fellipe.com>
2016-02-15 03:06:02 -0500

    remove old emails

--- git/git_rename_user_branch.sh
+++ git/git_rename_user_branch.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
- 
+
 git filter-branch --env-filter '
- 
+
 an="$GIT_AUTHOR_NAME"
 am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
- 
-if [ "$GIT_COMMITTER_EMAIL" = "davidson@corp.globo.com" ]
+
+if [ "$GIT_COMMITTER_EMAIL" = "OLD_USER" ]
 then
-    cn="Davidson Fellipe"
-    cm="email@fellipe.com"
+    cn="NEW_USER_NAME"
+    cm="NEW_USER_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "davidson@corp.globo.com" ]
+if [ "$GIT_AUTHOR_EMAIL" = "OLD_USER" ]
 then
-    an="Davidson Fellipe"
-    am="email@fellipe.com"
+    an="NEW_USER_NAME"
+    am="NEW_USER_EMAIL"
 fi
- 
+
 export GIT_AUTHOR_NAME="$an"
 export GIT_AUTHOR_EMAIL="$am"
 export GIT_COMMITTER_NAME="$cn"
