60d000010f6b563e615dec60b2e1ace5864929ce
Davidson Fellipe <email@fellipe.com>
2018-06-19 17:39:16 -0400

    chore(bpkg): Removing mongodb

--- brew/global.sh
+++ brew/global.sh
@@ -9,17 +9,6 @@ else
     msg_ok "OK"
 fi
 
-# mongo
-if which mongod &> /dev/null; then
-    msg_checking "mongodb"
-else
-    msg_install "mongodb" "brew install mongodb"
-    brew install mongodb --with-openssl
-    msg_ok "OK"
-fi
-
-ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
 sudo mkdir -p /data/db
 ls -ld /data/db/
 sudo chmod 0755 /data/db
-sudo chown mongod:mongod /data/db
